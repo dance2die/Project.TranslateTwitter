35e97a60ad0fa7a9543c4633cc138a1eb32fa796
dance2die <dance2die@hotmail.com>
2015-08-08 14:00:44 -0400

    checking into Gibhub because commit isn't showing up on github

--- Project.TranslateTwitter/Project.TranslateTwitter.Translator.Mstf.Demo/Program.cs
+++ Project.TranslateTwitter/Project.TranslateTwitter.Translator.Mstf.Demo/Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace Project.TranslateTwitter.Translator.Mstf.Demo
 {
-	class Program
+	public class Program
 	{
-		static void Main(string[] args)
+		public static void Main(string[] args)
 		{
+			Test1();
 		}
+
+		/// <remarks>
+		/// http://blogs.msdn.com/b/translation/p/gettingstarted2.aspx
+		/// </remarks>
+		private static void Test1()
+		{
+
+		}
+	}
+
+	public class AdmAccessToken
+	{
+		public string AccessToken { get; set; }
+		public string TokenType { get; set; }
+		public string ExpiresIn { get; set; }
+		public string Scope { get; set; }
 	}
 }
