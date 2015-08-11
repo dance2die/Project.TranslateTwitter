6984e5c59b71c383ec9c2456539f261b2650d210
dance2die <dontbothermewithspam@gmail.com>
2015-08-11 11:13:53 -0400

    Added a skeleton code for "LanguageLister"

---
+++ Project.TranslateTwitter/Project.TranslateTwitter.Translator/Microsoft/LanguageLister.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.TranslateTwitter.Translator.Microsoft
+{
+	public class LanguageLister : LanguageParent
+	{
+		protected override string MethodName => "GetLanguagesForTranslate";
+
+		public LanguageLister(IAuthenticationContext authenticationContext) 
+			: base(authenticationContext)
+		{
+		}
+
+		public List<string> GetSupportedLanguages()
+		{
+			List<string> result = new List<string>();
+			return result;
+		}
+
+		protected override string GetQueryString()
+		{
+			return string.Empty;
+		}
+	}
+}
