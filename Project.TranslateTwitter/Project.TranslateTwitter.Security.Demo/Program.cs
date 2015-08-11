6c27637d2a3c16173dc873be88a627974bda353b
dance2die <dontbothermewithspam@gmail.com>
2015-08-11 14:00:02 -0400

    added "Project.TranslateTwitter.Security.Demo" project to test connecting to twitter.

---
+++ Project.TranslateTwitter/Project.TranslateTwitter.Security.Demo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.TranslateTwitter.Security.Demo
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			TestSignInWithXAuth();
+		}
+
+		private static void TestSignInWithXAuth()
+		{
+			
+		}
+	}
+}
