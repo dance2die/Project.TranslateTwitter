7ccdae9f329fbdd34dab78b0e43944b919a0e8ac
dance2die <dontbothermewithspam@gmail.com>
2015-08-14 11:16:19 -0400

    It wasn't added to project due to VS bug

---
+++ Project.TranslateTwitter/Project.TranslateTwitter.Security.Demo/OAuthProperties.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Project.TranslateTwitter.Security.Demo
+{
+	public static class OAuthProperties
+	{
+		public static string Token => Environment.GetEnvironmentVariable("Project_TranslateTwitter.AccessToken", EnvironmentVariableTarget.User);
+		public static string TokenSecret => Environment.GetEnvironmentVariable("Project_TranslateTwitter.AccessTokenSecret", EnvironmentVariableTarget.User);
+		public static string ConsumerKey => Environment.GetEnvironmentVariable("Project_TranslateTwitter.ConsumerKey", EnvironmentVariableTarget.User);
+		public static string ConsumerKeySecret => Environment.GetEnvironmentVariable("Project_TranslateTwitter.ConsumerKeySecret", EnvironmentVariableTarget.User);
+	}
+}
