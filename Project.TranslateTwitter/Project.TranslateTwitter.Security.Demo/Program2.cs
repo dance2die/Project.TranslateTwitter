87765d5f29ae45699a893eb62f2fb0901218a7c7
dance2die <dontbothermewithspam@gmail.com>
2015-08-17 15:45:46 -0400

    For some reason some files were not committed to GitHub. Need to merge this

--- Project.TranslateTwitter/Project.TranslateTwitter.Security.Demo/Program2.cs
+++ Project.TranslateTwitter/Project.TranslateTwitter.Security.Demo/Program2.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +13,24 @@ namespace Project.TranslateTwitter.Security.Demo
 		{
 			// Need to learn how to create a signature first.
 			// https://dev.twitter.com/oauth/overview/creating-signatures
+			TestCreateSignature();
 		}
-	}
+
+		private static void TestCreateSignature()
+		{
+			string httpMethod = "GET";
+			string baseUrl = "";
+			Dictionary<string, string> requestParams = GetRequestParams();
+			var signature = CreateSignature(httpMethod, baseUrl, requestParams);
+			Console.WriteLine("Signature: {0}", signature);
+		}
+
+		private static string CreateSignature(
+			string httpMethod, string baseUrl, IDictionary<string, string> requestParams)
+		{
+			var result = string.Empty;
+
+			return result;
+		}
+    }
 }
