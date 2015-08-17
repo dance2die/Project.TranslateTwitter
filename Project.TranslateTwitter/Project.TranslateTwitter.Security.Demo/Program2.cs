5085ac38c504d158ca5a7fd66fe5649d45bb006e
dance2die <dance2die@hotmail.com>
2015-08-17 19:11:42 -0400

    Added Program2

---
+++ Project.TranslateTwitter/Project.TranslateTwitter.Security.Demo/Program2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.TranslateTwitter.Security.Demo
+{
+	public class Program2
+	{
+		public static void Main(string[] args)
+		{
+			// Need to learn how to create a signature first.
+			// https://dev.twitter.com/oauth/overview/creating-signatures
+			TestCreateSignature();
+		}
+
+		private static void TestCreateSignature()
+		{
+			string httpMethod = "GET";
+			string baseUrl = "";
+			Dictionary<string, string> requestParams = GetRequestParams();
+			var signature = CreateSignature(httpMethod, baseUrl, requestParams);
+			Console.WriteLine("Signature: {0}", signature);
+		}
+
+		private static string CreateSignature(
+			string httpMethod, string baseUrl, IDictionary<string, string> requestParams)
+		{
+			var result = string.Empty;
+
+			return result;
+		}
+	}
+}
