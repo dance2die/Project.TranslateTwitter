d9e1f02e5456956428985250e3fd028631eba9b9
dance2die <dance2die@hotmail.com>
2015-08-17 18:18:44 -0400

    Added Program3

---
+++ Project.TranslateTwitter/Project.TranslateTwitter.Security.Demo/Program3.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.TranslateTwitter.Security.Demo
+{
+	public class Program3
+	{
+		public static void Main(string[] args)
+		{
+			var screenName = "Pleasure54";	// This account uses korena & japanese
+
+			TestUserTimeLine(screenName);
+		}
+
+		private static void TestUserTimeLine(string screenName)
+		{
+			var oauth_token = OAuthProperties.AccessToken;
+			var oauth_token_secret = OAuthProperties.AccessTokenSecret;
+			var oauth_consumer_key = OAuthProperties.ConsumerKey;
+			var oauth_consumer_secret = OAuthProperties.ConsumerKeySecret;
+
+			var oauth_version = "1.0";
+			var oauth_signature_method = "HMAC-SHA1";
+			var oauth_nonce = Convert.ToBase64String(new ASCIIEncoding().GetBytes(DateTime.Now.Ticks.ToString()));
+			var timeSpan = DateTime.UtcNow- new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+			var oauth_timestamp = Convert.ToInt64(timeSpan.TotalSeconds).ToString();
+			var resource_url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+
+			var baseFormat = "oauth_consumer_key={0}&oauth_nonce={1}&oauth_signature_method={2}" +
+							 "&oauth_timestamp={3}&oauth_token={4}&oauth_version={5}";
+
+			var baseString = string.Format(baseFormat,
+				oauth_consumer_key,
+				oauth_nonce,
+				oauth_signature_method,
+				oauth_timestamp,
+				oauth_token,
+				oauth_version
+			);
+
+			baseString = string.Concat("POST&", Uri.EscapeDataString(resource_url),
+				"&", Uri.EscapeDataString(baseString));
+
+			var compositeKey = string.Concat(Uri.EscapeDataString(oauth_consumer_secret),
+				"&", Uri.EscapeDataString(oauth_token_secret));
+
+			string oauth_signature;
+			using (HMACSHA1 hasher = new HMACSHA1(Encoding.ASCII.GetBytes(compositeKey)))
+			{
+				oauth_signature = Convert.ToBase64String(
+					hasher.ComputeHash(Encoding.ASCII.GetBytes(baseString)));
+			}
+
+			var headerFormat =
+				"OAuth oauth_nonce=\"{0}\", oauth_signature_method=\"{1}\", " +
+				"oauth_timestamp=\"{2}\", oauth_consumer_key=\"{3}\", " +
+				"oauth_token=\"{4}\", oauth_signature=\"{5}\", " +
+				"oauth_version=\"{6}\"";
+
+			var authHeader = string.Format(headerFormat,
+				Uri.EscapeDataString(oauth_nonce),
+				Uri.EscapeDataString(oauth_signature_method),
+				Uri.EscapeDataString(oauth_timestamp),
+				Uri.EscapeDataString(oauth_consumer_key),
+				Uri.EscapeDataString(oauth_token),
+				Uri.EscapeDataString(oauth_signature),
+				Uri.EscapeDataString(oauth_version)
+				);
+
+
+
+			ServicePointManager.Expect100Continue = false;
+
+			var request = (HttpWebRequest)WebRequest.Create(resource_url);
+			request.Headers.Add("Authorization", authHeader);
+			request.Method = "POST";
+			request.ContentType = "application/x-www-form-urlencoded";
+
+			return request;
+		}
+	}
+}
---
+++ Project.TranslateTwitter/Project.TranslateTwitter.Translator/Microsoft/LanguageLookup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.TranslateTwitter.Translator.Microsoft
+{
+	public class LanguageLookup : LanguageParent
+	{
+		protected override string MethodName => "GetLanguagesForTranslate";
+
+		public LanguageLookup(IAuthenticationContext authenticationContext) 
+			: base(authenticationContext)
+		{
+		}
+
+		protected override string GetRequestUriQueryString()
+		{
+			return string.Empty;
+		}
+
+
+	}
+}
