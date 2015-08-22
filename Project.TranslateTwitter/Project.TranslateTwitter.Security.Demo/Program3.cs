239c881636ba3696d322489d0e5b41a235366ec8
dance2die <dance2die@hotmail.com>
2015-08-22 14:55:39 -0400

    Removed unused references

--- Project.TranslateTwitter/Project.TranslateTwitter.Security.Demo/Program3.cs
+++ Project.TranslateTwitter/Project.TranslateTwitter.Security.Demo/Program3.cs
@@ -7,10 +7,7 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Net.Http.Headers;
-using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 
