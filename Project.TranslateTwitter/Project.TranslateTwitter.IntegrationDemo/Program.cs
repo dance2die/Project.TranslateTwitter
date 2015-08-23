2032d7da3bf24fa473f61f873c410be03b9b6397
dance2die <dance2die@hotmail.com>
2015-08-22 23:36:09 -0400

    Correctly retrieves timeline

--- Project.TranslateTwitter/Project.TranslateTwitter.IntegrationDemo/Program.cs
+++ Project.TranslateTwitter/Project.TranslateTwitter.IntegrationDemo/Program.cs
@@ -1,8 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Project.TranslateTwitter.Security;
 
 namespace Project.TranslateTwitter.IntegrationDemo
 {
@@ -10,7 +16,55 @@ namespace Project.TranslateTwitter.IntegrationDemo
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			// Retrieve Tweet timeline.
+			dynamic timeline = GetTweetTimeline();
+
+			// Pick a non-English tweet and translate it
+			//dynamic nonEnglishTweet = GetNonEnglishTweet(timeline);
+			//string translatedTweet = TranslateTweet(nonEnglishTweet);
+
+			Console.Write("Press ENTER to continue...");
+			Console.ReadLine();
 		}
+
+		private static dynamic GetTweetTimeline()
+		{
+			string screenName = "dance2die";
+			var request = GetTimelineRequest(new AuthenticationContext(), screenName);
+
+			using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+			using (Stream dataStream = response.GetResponseStream())
+			{
+				//Open the stream using a StreamReader for easy access.
+				StreamReader reader = new StreamReader(dataStream);
+				//Read the content.
+				string responseFromServer = reader.ReadToEnd();
+				dynamic dynamicObject = JsonConvert.DeserializeObject<List<ExpandoObject>>(
+					responseFromServer, new ExpandoObjectConverter());
+				return dynamicObject;
+			}
+		}
+
+		private static HttpWebRequest GetTimelineRequest(AuthenticationContext authenticationContext, string screenName)
+		{
+			var requestBuilder = new RequestBuilder(authenticationContext);
+			var requestParameters = new TimelineRequestParameters(authenticationContext)
+			{
+				ScreenName = screenName,
+				Count = "10",
+			};
+
+			return requestBuilder.GetRequest(requestParameters);
+		}
+
+		//private static dynamic GetNonEnglishTweet(dynamic timeline)
+		//{
+			
+		//}
+
+		//private static string TranslateTweet(dynamic nonEnglishTweet)
+		//{
+			
+		//}
 	}
 }
