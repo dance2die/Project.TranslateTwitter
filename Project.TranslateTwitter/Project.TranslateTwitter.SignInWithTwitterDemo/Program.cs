77cad601785e846cf6db8a03dff199043a7f3414
dance2die <dance2die@hotmail.com>
2015-08-24 15:39:49 -0400

    Added "SignInWithTwitterDemo" project

---
+++ Project.TranslateTwitter/Project.TranslateTwitter.SignInWithTwitterDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.TranslateTwitter.SignInWithTwitterDemo
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+
+		}
+	}
+}
