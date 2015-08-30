206eede623980277af42eaad9d5805f01392ad38
dance2die <dance2die@hotmail.com>
2015-08-30 10:02:00 -0400

    "ValidateRequestTokenOAuthSignature" passes finally.

--- Project.TranslateTwitter/Project.TranslateTwitter.Integration.Test/OAuthSignatureTest.cs
+++ Project.TranslateTwitter/Project.TranslateTwitter.Integration.Test/OAuthSignatureTest.cs
@@ -61,6 +61,33 @@ namespace Project.TranslateTwitter.Integration.Test
 			Assert.Equal(oauthSignature, "tnnArxj06cWHq44gCs1OSKk/jLY=");
 		}
 
+		/// <summary>
+		/// Compare OAuth Signature to be generated for Request Token
+		/// </summary>
+		/// <remarks>https://dev.twitter.com/oauth/reference/post/oauth/request_token</remarks>
+		[Fact]
+		public void ValidateRequestTokenOAuthSignature()
+		{
+			var authenticationContext = new AuthenticationContext();
+			authenticationContext.AccessToken = null;
+			authenticationContext.AccessTokenSecret = null;
 
+			// SUT = System Under Test
+			var sut = new OAuthSignatureBuilder(authenticationContext);
+
+			RequestParameters requestParameters = new RequestTokenRequestParameters(authenticationContext, "oob");
+			requestParameters.Headers.Add("oauth_callback", "oob");
+			requestParameters.OAuthNonce = "e0d92fb7d5264baf85321de413176e9d";
+			requestParameters.OAuthTimestamp = "1440899892";
+
+			string baseString = sut.GetSignatureBaseString(requestParameters);
+			const string expectedBaseString = "POST&https%3A%2F%2Fapi.twitter.com%2Foauth%2Frequest_token&oauth_callback%3Doob%26oauth_consumer_key%3DG572rfcAFGznOnMZ2DDgCaBJV%26oauth_nonce%3De0d92fb7d5264baf85321de413176e9d%26oauth_signature_method%3DHMAC-SHA1%26oauth_timestamp%3D1440899892%26oauth_version%3D1.0";
+			Assert.Equal(expectedBaseString, baseString);
+
+			string oauthSignature = sut.CreateSignature(requestParameters);
+
+			// Second paramter from Twitter documentation: https://dev.twitter.com/oauth/overview/creating-signatures
+			Assert.Equal(oauthSignature, "NIkCr9R68INkZ87vt9D3N6Gb8BY=");
+		}
 	}
 }
--- Project.TranslateTwitter/Project.TranslateTwitter.Integration.Test/TestAuthenticationContext.cs
+++ Project.TranslateTwitter/Project.TranslateTwitter.Integration.Test/TestAuthenticationContext.cs
@@ -6,7 +6,7 @@ namespace Project.TranslateTwitter.Integration.Test
 	{
 		public string ConsumerKey => "xvz1evFS4wEEPTGEFPHBog";
 		public string ConsumerKeySecret => "kAcSOqF21Fu85e7zjz7ZN2U4ZRhfV3WpwPAoE3Z7kBw";
-		public string AccessToken => "370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb";
-		public string AccessTokenSecret => "LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE";
+		public string AccessToken { get; set; } = "370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb";
+		public string AccessTokenSecret { get; set; } = "LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE";
 	}
 }
